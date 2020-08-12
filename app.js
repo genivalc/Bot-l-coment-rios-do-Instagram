ab9c95fe2221d469abbadb4a5a4e90054320ef5c
genivalc <genivalcandeiadon.neto@gmail.com>
2020-08-11 23:59:19 -0300

    Projeto criado

---
+++ app.js
@@ -0,0 +1,18 @@
+//Ler a página do INsta
+
+//Pegar os comentários / @
+
+const fakeArrobas = [
+    '@neto',
+    '@neto',
+    '@Anderson',
+    '@rquel'
+]
+
+//contar arrobas repetidas
+
+function count(arrobas) {
+
+}
+
+//Ordenar
