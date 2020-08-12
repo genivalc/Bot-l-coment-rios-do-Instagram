9b4345a49a41ede90bee9fccbd2869f1428a44f1
genivalc <genivalcandeiadon.neto@gmail.com>
2020-08-12 01:29:39 -0300

    Projeto finalizado

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
-//Ler a página do INsta
+const puppeteer = require('puppeteer')
 
-//Pegar os comentários / @
 
-const fakeArrobas = [
-    '@neto',
-    '@neto',
-    '@Anderson',
-    '@rquel'
-]
 
-//contar arrobas repetidas
+async function start() {
+
+    async function loadMore(page, selector) {
+        const moreButton = await page.$(selector)
+        if (moreButton) {
+            console.log("Botão clicado")
+            await moreButton.click()
+            await page.waitFor(selector, { timeout: 3000 }).catch(() => {console.log("timeout")})
+            await loadMore(page, selector)
+        }
+    }
+
+    async function getComments(page, selector) {
+        const comments = await page.$$eval(
+            selector, links => links.map(
+                link => link.innerText))
+        return comments
+    }
+
+
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    page.goto('//aqui colocar  o link do post do Instagram')
+
+    
+    await loadMore(page, 'Nome da classe do Botão de carregamento do instagram');
+    const comments = await getComments(page, 'nome da classe do arroba do comentario')
+    const counted = count(comments)
+    const sorted = sort(counted)
+    sorted.forEach(arroba => { console.log(arroba)})
+
+    await browser.close()
+}
+
+
 
 function count(arrobas) {
     const count = {}
@@ -17,19 +44,15 @@ function count(arrobas) {
     return count
 }
 
-console.log(count(fakeArrobas))
 
-//Ordenar
 
 function sort(counted) {
-    const entries = []
-
-    for(prop in counted){
-        entries.push([prop, counted[prop]])
-    }
+    const entries = Object.entries(counted)
     const sorted = entries.sort((a, b) => b[1] - a[1])
-    console.log(sorted)
+    return sorted
 
 }
 
-sort(count(fakeArrobas))
+
+start()
+
